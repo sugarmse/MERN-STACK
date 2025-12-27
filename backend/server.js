0b6d26fa9a37a87f07073227db29c24dae579ca1
sugarmse <info.sarthakshakya@gmail.com>
2025-12-27 22:44:59 +0545

    code optimization

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,5 @@ app.get('/',(req,res)=>{
 //listen for requests
 app.listen(process.env.PORT, () => {
     console.log('listening for requests on port 4000!!!',process.env.PORT);
-    console.log("test");
 })
 
